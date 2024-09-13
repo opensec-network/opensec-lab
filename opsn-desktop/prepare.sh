24d63648d351c4da7b66191db5f56a148d11c358
Cesar Araque <cesar.araque@gmail.com>
2024-09-12 23:34:53 -0400

    Ajustando una vez mas la salida de los wget en preprare

--- opsn-desktop/prepare.sh
+++ opsn-desktop/prepare.sh
@@ -1,4 +1,4 @@
-wget --show-progress -O opsn-background.jpg https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/opsn-desktop/opsn-background.jpg
-wget --show-progress -O init.sh https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/opsn-desktop/init.sh
-wget --show-progress -O custom-init.sh https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/opsn-desktop/custom-init.sh
+wget -q --show-progress -O opsn-background.jpg https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/opsn-desktop/opsn-background.jpg
+wget -q --show-progress -O init.sh https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/opsn-desktop/init.sh
+wget -q --show-progress -O custom-init.sh https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/opsn-desktop/custom-init.sh
 chmod +x *.sh
