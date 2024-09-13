5d8dbfcd51392c747fc9e0631695d3cbd8c86ed2
Cesar Araque <cesar.araque@gmail.com>
2024-09-12 23:13:30 -0400

    Corrigiendo las rutas en prepare de opsn-desktop

--- opsn-desktop/prepare.sh
+++ opsn-desktop/prepare.sh
@@ -1,3 +1,3 @@
 wget -O opsn-background.jpg https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/opsn-desktop/opsn-background.jpg
 wget -O init.sh https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/opsn-desktop/init.sh
-wget -O custom-init.sh https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/custom-init.sh
+wget -O custom-init.sh https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/opsn-desktop/custom-init.sh
