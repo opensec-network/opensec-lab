8e76e29806dce5daa847086007037a84e5665d5f
Cesar Araque <cesar.araque@gmail.com>
2024-09-12 23:10:57 -0400

    Agregando la opción a wget para que sobreescriba en el prepare de opsn-desktop

--- opsn-desktop/prepare.sh
+++ opsn-desktop/prepare.sh
@@ -1,3 +1,3 @@
-wget https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/opsn-desktop/opsn-background.jpg
-wget https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/opsn-desktop/init.sh
-wget https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/custom-init.sh
+wget -O opsn-background.jpg https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/opsn-desktop/opsn-background.jpg
+wget -O init.sh https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/opsn-desktop/init.sh
+wget -O custom-init.sh https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/custom-init.sh
