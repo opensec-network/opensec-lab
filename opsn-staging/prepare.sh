dabe94dffad19597141b0e81c67e673e037220a9
Alejandro <admin@opensec.network>
2024-11-08 12:08:55 -0500

    Arreglando errores de Staging

--- opsn-staging/prepare.sh
+++
@@ -1,4 +0,0 @@
-wget -q --show-progress -O opsn-background.jpg https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/opsn-staging/opsn-background.jpg
-wget -q --show-progress -O init.sh https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/opsn-staging/init.sh
-wget -q --show-progress -O custom-init.sh https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/opsn-staging/custom-init.sh
-chmod +x *.sh
