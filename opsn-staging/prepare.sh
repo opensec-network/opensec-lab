dd70ea02611e7267a4c4daa46070be758eb255a0
Alejandro <admin@opensec.network>
2024-11-08 11:05:55 -0500

    Preparando el staging

---
+++ opsn-staging/prepare.sh
@@ -0,0 +1,4 @@
+wget -q --show-progress -O opsn-background.jpg https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/staging/opsn-background.jpg
+wget -q --show-progress -O init.sh https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/staging/init.sh
+wget -q --show-progress -O custom-init.sh https://raw.githubusercontent.com/opensec-network/opensec-lab/refs/heads/main/staging/custom-init.sh
+chmod +x *.sh
